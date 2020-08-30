0cc552cbdb8b0c09a4614a1832a42f7ad2738acd
Georgi Dimitranov <g_dimitranov@abv.bg>
2020-08-31 02:00:43 +0300

    Updates

---
+++ precache-manifest.8e4993795d844ce6461cdded78acea25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ff7a25204a63509c15eebbe4451339",
+    "url": "/movie-bob/index.html"
+  },
+  {
+    "revision": "9412a337d35a11b14662",
+    "url": "/movie-bob/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "64e213e8ccd3276bdff7",
+    "url": "/movie-bob/static/js/2.87d27581.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-bob/static/js/2.87d27581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9412a337d35a11b14662",
+    "url": "/movie-bob/static/js/main.b4706caa.chunk.js"
+  },
+  {
+    "revision": "de71c3454f0282faf9f7",
+    "url": "/movie-bob/static/js/runtime-main.e2475899.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/movie-bob/static/media/logo.ee7cd8ed.svg"
+  }
+]);
